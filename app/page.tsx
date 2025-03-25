a3175671ccfe3f43a08167e1cb2fc65ab46190de
Taiwei Tuan <taiweituan@gmail.com>
2025-03-25 10:57:52 -0500

    minor update

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,6 @@ import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
   title: "State Management Libraries Comparison by Taiwei",
-  twitter: {
-    card: "summary_large_image",
-  },
-  openGraph: {
-    url: "https://next-enterprise.vercel.app/",
-    images: [
-      {
-        width: 1200,
-        height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
-      },
-    ],
-  },
 }
 
 export default function Web() {
