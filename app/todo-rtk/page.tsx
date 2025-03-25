ab34accdcd0813f34dd25ea7319741c9f4b9cf00
Taiwei Tuan <taiweituan@gmail.com>
2025-03-25 10:08:35 -0500

    minor update

--- app/todo-rtk/page.tsx
+++ app/todo-rtk/page.tsx
@@ -63,14 +63,22 @@ const todoSlice = createSlice({
   } as TodoState,
   reducers: {
     addTodo: (state, action: PayloadAction<string>) => {
-      state.todos.push({ id: Date.now(), text: action.payload, completed: false })
+      return {
+        ...state,
+        todos: [...state.todos, { id: Date.now(), text: action.payload, completed: false }],
+      }
     },
     toggleTodo: (state, action: PayloadAction<number>) => {
-      const todo = state.todos.find((todo) => todo.id === action.payload)
-      if (todo) todo.completed = !todo.completed
+      return {
+        ...state,
+        todos: state.todos.map((todo) => (todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo)),
+      }
     },
     deleteTodo: (state, action: PayloadAction<number>) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      }
     },
   },
 })
